9fd9e20c8215fbf9f5aecef63fb13032c86094a8
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-07 23:08:14 +0300

    Created auth store module. Created folder for store modules. Configured axios interceptors for authentication purposes.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,11 +1,32 @@
 import axios from 'axios';
+import store from '@/store';
 
-export default axios.create({
+const axiosInstance = axios.create({
     baseURL: process.env.VUE_APP_API_BASE_URL,
 
     headers: {
         'Content-Type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest'
-    },
+    }
     // withCredentials: true
 });
+
+axiosInstance.interceptors.request.use(request => {
+    const token = store.getters['auth/token'];
+    if (token) {
+        request.headers.common.Authorization = `Bearer ${ token }`;
+    }
+
+    return request;
+});
+
+axiosInstance.interceptors.response.use(response => response, async error => {
+    const { status } = error.response;
+    if (status === 401 && store.getters['auth/check']) {
+        await store.dispatch('auth/logout');
+
+        // TODO: router.push({ name: 'login' });
+    }
+});
+
+export default axiosInstance;
---
+++ src/store/modules/auth.js
@@ -0,0 +1,58 @@
+import axios from '@/plugins/axios';
+import Cookies from 'js-cookie';
+
+export default {
+    namespaced: true,
+
+    state: {
+        user: null,
+        token: Cookies.get('token')
+    },
+    getters: {
+        user: state => state.user,
+
+        token: state => state.token,
+
+        check: state => state.user !== null,
+
+        roleName: state => state.user?.role.name,
+        isAdmin: (state, getters) => getters.roleName === 'admin'
+    },
+    mutations: {
+        fetchUserSuccess(state, user) {
+            state.user = user;
+        },
+        fetchUserFailure(state) {
+            state.token = null;
+            Cookies.remove('token');
+        },
+
+        saveToken(state, token) {
+            state.token = token;
+            Cookies.set('token', token);
+        },
+        logout(state) {
+            state.user = null;
+            state.token = null;
+
+            Cookies.remove('token');
+        }
+    },
+    actions: {
+        saveToken({ commit }, token) {
+            commit('saveToken', token);
+        },
+        async fetchUser({ commit }) {
+            try {
+                const { data } = await axios.get('/auth/current');
+
+                commit('fetchUserSuccess', data);
+            } catch (e) {
+                commit('fetchUserFailure');
+            }
+        },
+        async logout({ commit }) {
+            commit('logout');
+        }
+    }
+};
