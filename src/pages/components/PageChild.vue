d618385bc032f1c30522185b1324d24acfb1c938
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-10 01:15:53 +0300

    Created component-wrapper for nested pages with transition.

---
+++ src/pages/components/PageChild.vue
@@ -0,0 +1,24 @@
+<template>
+    <transition name="fade" mode="out-in">
+        <slot>
+            <router-view/>
+        </slot>
+    </transition>
+</template>
+
+<script>
+export default {
+    name: 'PageChild'
+};
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .15s
+}
+.fade-enter,
+.fade-leave-to {
+    opacity: 0
+}
+</style>
