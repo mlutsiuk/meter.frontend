1ca03904b333733228e9e6a93fd92519619dceea
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-09 04:20:20 +0300

    Installed vee-validate package.

---
+++ src/plugins/vee-validate.js
@@ -0,0 +1,47 @@
+import { extend, setInteractionMode } from 'vee-validate';
+import { email, max, max_value, min, min_value, numeric, required } from 'vee-validate/dist/rules';
+
+setInteractionMode('eager');
+
+extend('required', {
+    ...required,
+    message: 'Не може бути порожнє',
+    computesRequired: true,
+
+    validate(value) {
+        return {
+            required: true,
+            valid: ['', null, undefined].indexOf(value) === -1
+        };
+    }
+});
+
+extend('email', {
+    ...email,
+    message: 'Неправильний email'
+});
+
+extend('max', {
+    ...max,
+    message: 'Не може містити більше {length} символів'
+});
+
+extend('max_value', {
+    ...max_value,
+    message: 'Не можи бути більше за {max}'
+});
+
+extend('min', {
+    ...min,
+    message: 'Не може містити менше {length} символів'
+});
+
+extend('min_value', {
+    ...min_value,
+    message: 'Не можи бути менше за {min}'
+});
+
+extend('numeric', {
+    ...numeric,
+    message: 'Може містити виключно цифри'
+});
