a23169fd4cf574e051760c79a1cec56f404ce019
Anna Romaniuk <80681158+AnechkaR@users.noreply.github.com>
2022-06-09 18:04:56 +0300

    Redesigned 404 error page

--- src/pages/Error/NotFound/NotFoundPage.vue
+++ src/pages/Error/NotFound/NotFoundPage.vue
@@ -1,19 +1,21 @@
 <template>
-    <v-container class="text-center">
-        <h1 id="error-page-caption" class="text-h1">404</h1>
-        <div class="text--secondary text-subtitle-1">Сторінку не знайдено. Халепа!</div>
+    <div class="default-layout-full-page d-flex justify-center align-center">
+        <div class="d-flex align-center flex-column">
+            <h1 id="error-page-caption" class="text-h1">404</h1>
+            <div class="text--secondary text-subtitle-1">Сторінку не знайдено. Халепа!</div>
 
-        <v-btn
-            @click="$router.push({ name: 'home' })"
-            class="mt-6"
-            color="accent"
-            elevation="2"
-            large
-            outlined
-        >
-            На базу
-        </v-btn>
-    </v-container>
+            <v-btn
+                @click="$router.push({ name: 'home' })"
+                class="mt-6"
+                color="accent"
+                elevation="2"
+                large
+                outlined
+            >
+                На базу
+            </v-btn>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -23,6 +25,10 @@ export default {
 </script>
 
 <style scoped>
+.default-layout-full-page {
+    height: calc(100vh - 64px);
+}
+
 #error-page-caption {
     font-family: Nunito, sans-serif !important;
 }
