54934fc71f0676ca676b73a25c343089de66defc
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-12 00:34:31 +0300

    Fixed user data get request.

--- src/pages/Auth/Login/LoginPage.vue
+++ src/pages/Auth/Login/LoginPage.vue
@@ -119,7 +119,7 @@ export default {
                     email: this.email,
                     password: this.password
                 });
-                token = response.access_token;
+                token = response.data.access_token;
             } catch (e) {
                 this.loading = false;
 
