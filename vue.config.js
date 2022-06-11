6d4f4d9e12df087c32548470e8c42a41d40b80d4
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-11 15:56:40 +0300

    Updated vue.config.js.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: process.env.VUE_APP_NAME,
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        }
+    },
     transpileDependencies: [
         'vuetify'
     ]
