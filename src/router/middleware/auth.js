0874fc5a91ff4ff7c0bff7c552450eba86eefa8d
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-11 04:42:20 +0300

    Added error message output to auth middleware.

--- src/router/middleware/auth.js
+++ src/router/middleware/auth.js
@@ -5,7 +5,11 @@ export default async(to, from, next) => {
     if(!store.getters['auth/check']) {
         Cookies.set('intended_url', to.path);
 
-        next({ name: 'login' });    //TODO: Show snackbar with error message
+        store.commit('message/push', {
+            text: 'Для перегляду необхідно увійти.',
+            color: 'warning'
+        });
+        next({ name: 'login' });
     } else {
         next();
     }
