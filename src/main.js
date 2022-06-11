97e52290b74a648ce5e25a052b000431f84fb78a
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-12 02:56:01 +0300

    Added mitt package.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import emitter from './plugins/emitter';
 
 import './plugins/vee-validate';
 
@@ -10,6 +11,8 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$mitt = emitter;
 
 new Vue({
     ...App,
