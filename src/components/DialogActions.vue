a865910cb8370943a424abcfa81eadefcef57e6e
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-13 17:09:18 +0300

    Created DialogActions component.

---
+++ src/components/DialogActions.vue
@@ -0,0 +1,95 @@
+<template>
+    <v-card-actions>
+        <v-spacer v-if="!stickToLeft"/>
+
+        <template v-if="closeOnly">
+            <v-btn
+                @click="$emit('close')"
+                :loading="loading"
+                :disabled="loading"
+                text
+            >
+                {{ closeText }}
+            </v-btn>
+        </template>
+        <template v-else>
+            <v-btn
+                v-if="!hideCancel"
+                @click="$emit('cancel')"
+                :disabled="loading"
+                :loading="isLoadingCancelButton"
+                :color="cancelColor"
+                text
+            >
+                {{ cancelText }}
+            </v-btn>
+            <v-btn
+                v-if="!hideConfirm"
+                @click="$emit('confirm')"
+                :disabled="loading"
+                :loading="loading"
+                :color="confirmColor"
+                text
+            >
+                {{ confirmText }}
+            </v-btn>
+        </template>
+    </v-card-actions>
+</template>
+
+<script>
+export default {
+    name: 'DialogActions',
+    props: {
+        loading: {
+            type: Boolean,
+            default: false
+        },
+        stickToLeft: {
+            type: Boolean,
+            default: false
+        },
+        closeOnly: {
+            type: Boolean,
+            default: false
+        },
+        closeText: {
+            type: String,
+            default: 'Закрити'
+        },
+        hideCancel: {
+            type: Boolean,
+            default: false
+        },
+        hideConfirm: {
+            type: Boolean,
+            default: false
+        },
+        cancelColor: {
+            type: String,
+            default: ''
+        },
+        confirmColor: {
+            type: String,
+            default: 'primary'
+        },
+        cancelText: {
+            type: String,
+            default: 'Скасувати'
+        },
+        confirmText: {
+            type: String,
+            default: 'Зберегти'
+        }
+    },
+    computed: {
+        isLoadingCancelButton() {
+            if(this.loading === false) {
+                return false;
+            }
+            return this.hideConfirm;
+            // If confirm button is hidden, then loading animation must be shown in a cancel button
+        }
+    }
+};
+</script>
