cd46984dea07ffd74aa5e73d71127f779234e24d
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-14 21:44:38 +0300

    Implementing counter appearance edit in CounterAppearanceDialog.

--- src/components/Dialogs/Counters/Appearance/CounterAppearanceDialog.vue
+++ src/components/Dialogs/Counters/Appearance/CounterAppearanceDialog.vue
@@ -3,50 +3,63 @@
         @click:outside="close()"
         @keydown.esc="close()"
         v-model="active"
-        max-width="500px"
+        max-width="380px"
         no-click-animation
         persistent
+        scrollable
     >
         <v-card>
             <v-card-title>
                 Зовнішній вигляд лічильника
             </v-card-title>
-            <v-card-text>
-                <v-row>
-                    <v-col cols="6">
-                        <!-- TODO: Create component for icons selection -->
+            <v-card-text class="themed-scrollbar">
+                <!-- TODO: Create component for icons selection -->
+                <v-card class="mb-4" flat outlined>
+                    <v-card-text class="d-flex flex-column">
                         <v-autocomplete
+                            v-model="selectedIconId"
                             :items="icons"
                             item-value="id"
                             item-text="code"
+                            auto-select-first
+                            filled
+                            dense
                         >
                             <template #selection="{ item }">
-                                <v-icon v-text="item.code"/>
+                                <v-icon class="mr-4" v-text="item.code" :color="color"/>
                                 {{ item.code }}
                             </template>
 
-                            <template #item="{ item }">
-                                <v-list-item-icon>
-                                    <v-icon v-text="item.code"/>
-                                </v-list-item-icon>
-
-                                <v-list-item-content>
-                                    <v-list-item-action-text v-text="item.code"/>
-                                </v-list-item-content>
+                            <template #item="{ item, on, attrs }">
+                                <v-list-item v-on="on" v-bind="attrs">
+                                    <v-list-item-icon>
+                                        <v-icon v-text="item.code" :color="color"/>
+                                    </v-list-item-icon>
+                                    <v-list-item-content>
+                                        <v-list-item-action-text v-text="item.code"/>
+                                    </v-list-item-content>
+                                </v-list-item>
                             </template>
                         </v-autocomplete>
-                    </v-col>
-                    <v-col cols="9">
+
                         <v-color-picker
                             v-model="color"
+                            class="align-self-center"
+                            swatches-max-height="150"
+                            hide-sliders
                             hide-canvas
                             hide-inputs
                             hide-mode-switch
                             show-swatches
-                            swatches-max-height="150"
                         />
-                    </v-col>
-                </v-row>
+                    </v-card-text>
+                </v-card>
+
+                <counters-grid-cell
+                    :color="color"
+                    :icon="iconCode"
+                    :title="title"
+                />
             </v-card-text>
 
             <dialog-actions
@@ -64,17 +77,19 @@
 import { DIALOG_SHOW_COUNTER_APPEARANCE } from '@/components/Dialogs/events';
 import dialogMixin from '@/components/Dialogs/dialogMixin';
 import DialogActions from '@/components/DialogActions';
+import CountersGridCell from '@/pages/Home/Dashboard/components/CountersGridCell';
 
 export default {
     name: 'CounterAppearanceDialog',
     mixins: [ dialogMixin ],
     components: {
+        CountersGridCell,
         DialogActions
     },
     data: () => ({
         activationEvent: DIALOG_SHOW_COUNTER_APPEARANCE,
 
-        title: '',
+        title: 'Газ',
         icons: [
             {
                 id: 1,
@@ -89,6 +104,7 @@ export default {
                 code: 'mdi-lightbulb'
             }
         ],
+        selectedIconId: null,
 
         color: '#4FC3F7'
     }),
@@ -104,6 +120,11 @@ export default {
                 text: 'Збережено',
                 color: 'success'
             });
+        }
+    },
+    computed: {
+        iconCode() {
+            return this.icons.find(icon => icon.id === this.selectedIconId)?.code;
         }
     }
 };
