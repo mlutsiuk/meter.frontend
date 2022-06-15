fa03a5f75f2af9ed5bee06bebc48fcd366c413c4
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-15 20:46:24 +0300

    Created CounterColorPicker component.

---
+++ src/components/Dialogs/Counters/components/CounterColorPicker.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-color-picker
+        v-model="color"
+        style="max-width: none"
+        swatches-max-height="150"
+        hide-sliders
+        hide-canvas
+        hide-inputs
+        hide-mode-switch
+        show-swatches
+    />
+</template>
+
+<script>
+export default {
+    name: 'CounterColorPicker',
+    props: {
+        value: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        color: {
+            get() {
+                return this.value;
+            },
+            set(val) {
+                this.$emit('input', val);
+            }
+        }
+    }
+};
+</script>
