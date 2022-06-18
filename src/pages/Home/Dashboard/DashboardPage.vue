3fc4bb1789ebfd0d452db782d054f79a1a00d91c
Anna Romaniuk <80681158+AnechkaR@users.noreply.github.com>
2022-06-18 18:38:26 +0300

    Add loader

--- src/pages/Home/Dashboard/DashboardPage.vue
+++ src/pages/Home/Dashboard/DashboardPage.vue
@@ -1,34 +1,39 @@
 <template>
     <v-container
-        @contextmenu.prevent="$refs.contextmenu.show"
+        @contextmenu.prevent="showContextMenu"
         class="pt-0"
         fluid
     >
 
-        <v-btn
-            v-if="groups.length === 0"
-            @contextmenu.prevent.stop="$mitt.emit('context-menu-hide')"
-            @click="showGroupCreateDialog()"
-            style="min-height: 130px"
-            block
-            outlined
-            text
+        <v-skeleton-loader
+            :loading="loading"
+            type="card-heading, image"
         >
-            <v-icon>mdi-plus</v-icon>
-            Додати першу групу
-        </v-btn>
-        <group-card
-            v-else
-            v-for="group in groups"
-            :key="group.id"
-            :group="group"
-        />
+            <v-btn
+                v-if="groups.length === 0"
+                @contextmenu.prevent.stop="$mitt.emit('context-menu-hide')"
+                @click="showGroupCreateDialog()"
+                style="min-height: 130px"
+                block
+                outlined
+                text
+            >
+                <v-icon>mdi-plus</v-icon>
+                Додати першу групу
+            </v-btn>
+            <group-card
+                v-else
+                v-for="group in groups"
+                :key="group.id"
+                :group="group"
+            />
 
-        <context-menu ref="contextmenu">
-            <context-menu-item icon="mdi-home-group-plus" @click="showGroupCreateDialog()">
-                Створити групу
-            </context-menu-item>
-        </context-menu>
+            <context-menu ref="contextmenu">
+                <context-menu-item icon="mdi-home-group-plus" @click="showGroupCreateDialog()">
+                    Створити групу
+                </context-menu-item>
+            </context-menu>
+        </v-skeleton-loader>
     </v-container>
 </template>
 
@@ -48,15 +53,24 @@ export default {
         GroupCard
     },
     data: () => ({
-        groups: []
+        groups: [],
+
+        loading: false
     }),
     methods: {
+        showContextMenu(event) {
+            if(this.loading) {
+                return;
+            }
+            this.$refs.contextmenu.show(event);
+        },
+
         showGroupCreateDialog() {
             showGroupCreateDialog(() => this.$mitt.emit(PAGE_DASHBOARD_RELOAD));
         },
 
-        reloadData() {
-            this.loadMyGroups();
+        async reloadData() {
+            await this.loadMyGroups();
         },
         async loadMyGroups() {
             let groups = (await axios.get('/dashboard/my')).data;
@@ -66,10 +80,12 @@ export default {
             this.groups = groups;
         }
     },
-    created() {
-        this.reloadData();
-
+    async created() {
         this.$mitt.on(PAGE_DASHBOARD_RELOAD, this.reloadData);
+
+        this.loading = true;
+        await this.reloadData();
+        this.loading = false;
     },
     beforeDestroy() {
         this.$mitt.off(PAGE_DASHBOARD_RELOAD, this.reloadData);
