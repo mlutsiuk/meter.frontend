dd266e162838820f1b38426f482f5119d8984c59
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-20 02:09:08 +0300

    Created admin panel navigation.

--- src/pages/Admin/AdminPage.vue
+++ src/pages/Admin/AdminPage.vue
@@ -1,10 +1,36 @@
 <template>
-    <h3 class="text-h3">AdminPage</h3>
+    <v-row class="pa-3">
+        <v-col
+            cols="3"
+            class="scrollable-container align-stretch"
+        >
+            <admin-navigation/>
+        </v-col>
 
+        <v-divider vertical/>
+
+        <v-col
+            cols="9"
+            class="scrollable-container d-flex align-stretch"
+        >
+            <page-child/>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
+import PageChild from '@/pages/components/PageChild';
+import AdminNavigation from './components/AdminNavigation';
+
 export default {
-    name: 'AdminPage'
+    name: 'AdminPage',
+    components: { AdminNavigation, PageChild }
 };
 </script>
+
+<style scoped>
+.scrollable-container {
+    overflow-y: scroll;
+    height: calc(100vh - 64px);
+}
+</style>
---
+++ src/pages/Admin/components/AdminNavigation.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-list>
+        <v-list-item @click="$router.push({ name: 'admin' })">
+            <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-title>Головна</v-list-item-title>
+        </v-list-item>
+
+        <v-divider/>
+
+        <v-list-item
+            @dblclick="$router.push({ name: 'admin.icons' })"
+            link
+        >
+
+            <v-list-item-icon>
+                <v-icon>mdi-star-circle</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+                <v-list-item-title>Іконки</v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+    </v-list>
+</template>
+
+<script>
+export default {
+    name: 'AdminNavigation'
+};
+</script>
